4df6571b064f61793c44e6dee414ea6270aa2d3d
¨alec¨ <alejo1ibarra@gmail.com>
2023-08-27 17:57:55 -0500

    :recycle: update web server connection

--- main.py
+++ main.py
@@ -10,8 +10,8 @@ app = get_asgi_application()
 if __name__ == "__main__":
     uvicorn.run(
         "main:app",
-        host="127.0.0.1",
-        port=8000,
+        host="0.0.0.0",
+        port=8080,
         reload=settings.DEBUG,
         workers=1,
     )
